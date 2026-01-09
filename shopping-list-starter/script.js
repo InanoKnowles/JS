48fb0cf575f6f63a2edb4623c6af1a5fbac6b3c0
InanoKnowles <missinanoknowles@gmail.com>
2026-01-10 07:36:43 +1000

    shopping list completed

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,7 +1,27 @@
-const addItem = () => {
+let shoppingListItems = [];
 
+const addItem = () => {
+    let inputElement = document.getElementById("new-item-text");
+    let item = inputElement.value;
+
+    shoppingListItems = [...shoppingListItems, item];
+    updateItems();
+
+    inputElement.value = "";
 };
 
 const updateItems = () => {
+    let listElement = document.getElementById("shopping-list-items");
+    listElement.innerHTML = "";
 
+    for (const shoppingItem of shoppingListItems) {
+        let itemElement = document.createElement("li");
+        itemElement.innerText = shoppingItem;
+        listElement.appendChild(itemElement);
+    }
+};
+
+const clearList = () => {
+    shoppingListItems = [];
+    updateItems();
 };
